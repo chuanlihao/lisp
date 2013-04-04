5e3c8e681b4fa603c10717f204bd26f5ec174459
Chuanli Hao <chuanlihao@yahoo.com>
2013-04-04 15:25:45 +0800

    add generate-text.lisp

---
+++ ansi-common-lisp-book/generate-text.lisp
@@ -0,0 +1,53 @@
+; From book ANSI Common Lisp, chapter 8, GNU CLISP 2.4.n
+
+(defparameter *words* (make-hash-table :size 1000))
+
+(defconstant maxword 100)
+
+(defun read-text (pathname)
+  (with-open-file (s pathname :direction :input)
+    (let ((buffer (make-string maxword))
+          (pos 0))
+      (do ((c (read-char s nil :eof)
+              (read-char s nil :eof)))
+          ((equal c :eof))
+        (if (or (alpha-char-p c) (char= c #\'))
+            (progn
+              (setf (aref buffer pos) c)
+              (incf pos))
+            (progn
+              (unless (zerop pos)
+                (see (intern (string-upcase
+                               (subseq buffer 0 pos))))
+                (setf pos 0))
+              (let ((p (punc c)))
+                (if p (see p)))))))))
+
+(defun punc (c)
+  (case c
+    (#\. '|.|) (#\, '|,|) (#\; '|;|)
+    (#\! '|!|) (#\? '|?|)))
+
+(let ((prev '|.|))
+  (defun see (symb)
+    (format t "~A " symb)
+    (let ((pair (assoc symb (gethash prev *words*))))
+      (if (null pair)
+          (push (cons symb 1) (gethash prev *words*))
+          (incf (cdr pair))))
+    (setf prev symb)))
+
+
+(defun generate-text (n &optional (prev '|.|))
+  (if (zerop n)
+      (terpri)
+      (let ((next (random-next prev)))
+        (format t "~A " next)
+        (generate-text (1- n) next))))
+
+(defun random-next (prev)
+  (let* ((choices (gethash prev *words*))
+         (i (random (reduce #'+ choices :key #'cdr))))
+    (dolist (pair choices)
+      (if (minusp (decf i (cdr pair)))
+          (return (car pair))))))
