ab6f69fc8df90ed9499f8291f1e8cab3309e85e8
Chuanli Hao <chuanlihao@yahoo.com>
2013-06-30 22:19:40 +0800

    add other progs from chapter 05, book On Lisp

--- onlisp/ch05.lisp
+++ onlisp/ch05.lisp
@@ -36,3 +36,100 @@
       (if (funcall if x)
           (funcall then x)
           (if else (funcall else x)))))
+
+(defun fint (fn &rest fns)
+  (if (null fns)
+      fn
+      (let ((chain (apply #'fint fns)))
+        #'(lambda (x)
+            (and (funcall fn x) (funcall chain x))))))
+
+
+(defun lrec (rec &optional base)
+  (labels ((self (lst)
+             (if (null lst)
+                 (if (functionp base)
+                     (funcall base)
+                     base)
+                 (funcall rec (car lst)
+                              #'(lambda ()
+                                  (self (cdr lst)))))))
+    #'self))
+
+; copy-list
+(lrec #'(lambda (x f) (cons x (funcall f))))
+; remove-duplicates
+(lrec #'(lambda (x f) (adjoin x (funcall f))))
+; find-if, for some function fn
+(lrec #'(lambda (x f) (if (fn x) x (funcall f))))
+; some, for some function fn
+(lrec #'(lambda (x f) (or (fn x) (funcall f))))
+
+
+(defun our-copy-tree (tree)
+  (if (atom tree)
+      tree
+      (cons (our-copy-tree (car tree))
+            (our-copy-tree (cdr tree)))))
+
+(defun count-leaves (tree)
+  (if (atom tree)
+      (if tree 1 0)
+      (+ (count-leaves (car tree))
+         (count-leaves (cdr tree)))))
+
+(defun flatten (tree)
+  (if (atom tree)
+      (mklist tree)
+      (nconc (flatten (car tree))
+             (flatten (cdr tree)))))
+
+(defun mklist (obj)
+  (if (listp obj)
+      obj
+      (list obj)))
+
+(defun rfind-if (fn tree)
+  (if (atom tree)
+      (if tree
+          (and (funcall fn tree) tree))
+      (or (rfind-if fn (car tree))
+          (rfind-if fn (cdr tree)))))
+
+(defun ttrav (rec &optional (base #'identity))
+  (labels ((self (tree)
+                   (if (atom tree)
+                       (if (functionp base)
+                           (funcall base tree)
+                           base)
+                       (funcall rec 
+                                (self (car tree))
+                                (self (cdr tree))))))
+    #'self))
+
+; our-copy-tree
+(ttrav #'cons)
+; count-leaves
+(ttrav #'+ #'(lambda (x) (if x 1 0)))
+; flatten
+(ttrav #'nconc #'mklist)
+
+
+(defun trec (rec &optional (base #'identity))
+  (labels
+    ((self (tree)
+       (if (atom tree)
+           (if (functionp base)
+               (funcall base tree)
+               base)
+           (funcall rec
+                    tree
+                    #'(lambda ()
+                        (self (car tree)))
+                    #'(lambda ()
+                        (self (cdr tree)))))))
+   #'self))
+
+; rfind-if
+(trec #'(lambda (o l r) (or (funcall l) (funcall r)))
+      #'(lambda (leaf) (and (oddp leaf) leaf)))
