6e654b82ab8e3ec0256090a77236cfd67d3a5230
Chuanli Hao <chuanlihao@yahoo.com>
2013-07-02 23:24:51 +0800

    Add other progs from chapter 7, book On Lisp

--- onlisp/ch07.lisp
+++ onlisp/ch07.lisp
@@ -1,7 +1,7 @@
 ; programs from chapter 07, book On Lisp
 
-(defmacro me1 (expr)
-  `(macroexpand-1 ',expr))
+(defmacro mac (expr)
+  `(pprint (macroexpand-1 ',expr)))
 
 (defmacro nil! (var)
   (list 'setf var nil))
@@ -24,3 +24,81 @@
   `(do ()
        ((not ,test))
      ,@body))
+
+(defmacro our-dolist ((var list &optional result) &body body)
+  `(progn
+     (mapc #'(lambda (,var) ,@body)
+           ,list)
+     (let ((,var nil))
+       ,result)))
+
+(defmacro when-bind ((var expr) &body body)
+  `(let ((,var ,expr))
+     (when ,var
+       ,@body)))
+
+; draft for macro implementation
+(defmacro our-expander (name) `(get ,name 'expander))
+
+(defmacro our-defmacro (name params &body body)
+  (let ((g (gensym)))
+    `(progn
+       (setf (our-expander ',name)
+             #'(lambda (,g)
+                 (block ,name
+                   (destructuring-bind ,params (cdr ,g)
+                     ,@body))))
+       ',name)))
+
+(defun our-macroexpand-1 (expr)
+  (if (and (consp expr) (our-expander (car expr)))
+      (funcall (our-expander (car expr)) expr)
+      expr))
+
+
+(defmacro our-do (bindforms (test &rest result) &body body)
+  (let ((label (gensym)))
+    `(prog ,(make-initforms bindforms)
+       ,label
+       (if ,test
+           (return (progn ,@result)))
+       ,@body
+       (psetq ,@(make-stepforms bindforms))
+       (go ,label))))
+
+(defun make-initforms (bindforms)
+  (mapcar #'(lambda (b)
+              (if (consp b)
+                  (list (car b) (cadr b))
+                  (list b nil)))
+          bindforms))
+
+(defun make-stepforms (bindforms)
+  (mapcan #'(lambda (b)
+              (if (and (consp b) (third b))
+                  (list (car b) (third b))
+                  nil))
+          bindforms))
+
+
+(defmacro our-and (&rest args)
+  (case (length args)
+    (0 t)
+    (1 (car args))
+    (t `(if ,(car args)
+            (our-and ,@(cdr args))))))
+
+(defmacro our-andb (&rest args)
+  (if (null args)
+      t
+      (labels ((expander (rest)
+                 (if (cdr rest)
+                     `(if ,(car rest)
+                          ,(expander (cdr rest)))
+                     (car rest))))
+        (expander args))))
+
+
+(symbol-macrolet ((hi (progn (print "Howdy")
+                             1)))
+  (+ hi 2))
